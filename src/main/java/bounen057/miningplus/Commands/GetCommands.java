5bb53709264b4b710adb684e8351a2f63d4eeb54
Bounen057 <Bounen057@Gmail.com>
2019-07-30 05:08:36 +0900

    追加

---
+++ src/main/java/bounen057/miningplus/Commands/GetCommands.java
@@ -0,0 +1,11 @@
+package bounen057.miningplus.Commands;
+
+import bounen057.miningplus.MiningPlus;
+
+public class GetCommands {
+    private MiningPlus plugin;
+
+    public GetCommands(MiningPlus plugin) {
+        this.plugin = plugin;
+    }
+}
---
+++ src/main/java/bounen057/miningplus/MiningPlus.java
@@ -0,0 +1,17 @@
+package bounen057.miningplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiningPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
